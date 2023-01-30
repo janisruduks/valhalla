807bf334750d636b83df4e951be1e674c8588387
Erkyill <janis.ruduks157@gmail.com>
2023-01-31 01:05:52 +0200

    okay last update for today I promise

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer class="p-4 my-3 bg-black rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://janisruduks.space" class="hover:underline"> | Ar ❤️ veidojis Jānis Ruduks</a> | valhalla-lounge.lv.
         </span>
-            <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <ul class="hidden md:flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-black/0 md:dark:bg-transparent dark:border-black/20">
                 <li>
                 <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sākumlapa</a>
                 </li>
